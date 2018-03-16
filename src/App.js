08c891927239a95d284e4a86a1313cdeec633713
Vitaly Homonov <avg.idm@gmail.com>
2018-03-16 17:03:32 +0200

    add css-modules

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import { inject, observer } from "mobx-react";
-import "./App.css";
+import styles from "./App.module.scss";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <div>counter: {this.props.counter.counter}</div>
-        <button onClick={this.props.counter.inc}>inc</button>
-        <button onClick={this.props.counter.dec}>dec</button>
+        <button className={styles.red} onClick={this.props.counter.inc}>
+          inc
+        </button>
+        <button className={styles.blue} onClick={this.props.counter.dec}>
+          dec
+        </button>
       </div>
     );
   }
