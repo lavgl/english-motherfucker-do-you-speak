13925e652a1c12c8742e4ed02735bf156fa36bf0
Vitaly Homonov <avg.idm@gmail.com>
2018-03-16 11:24:34 +0200

    add prettier. fix eslint editor integration

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,7 @@
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import 'jest-enzyme';
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import "jest-enzyme";
 
-Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({
+  adapter: new Adapter(),
+});
